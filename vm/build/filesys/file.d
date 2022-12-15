17cacb3e3eea442ac707accd2e9769d9f9368dbf
Ubuntu <ubuntu@ip-172-31-14-84.ap-northeast-2.compute.internal>
2022-12-15 14:40:55 +0000

    feat: 유나는 많은 것을 배웠습니다.

--- vm/build/filesys/file.d
+++
@@ -1,6 +0,0 @@
-filesys/file.o: ../../filesys/file.c ../../include/filesys/file.h \
- ../../include/filesys/off_t.h ../../include/lib/stdint.h \
- ../../include/filesys/inode.h ../../include/lib/stdbool.h \
- ../../include/devices/disk.h ../../include/lib/inttypes.h \
- ../../include/lib/kernel/list.h ../../include/lib/stddef.h \
- ../../include/lib/debug.h ../../include/threads/malloc.h
